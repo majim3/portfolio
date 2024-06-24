b9f8031a1cd92a205c3ab61e6d9bd0223bb26fe1
Majim3 <leogong3@gmail.com>
2024-06-24 20:03:48 +0300

    buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './custom.scss'
 import {Button} from 'react-bootstrap'
 import './App.css'
 
---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Button} from 'react-bootstrap'
+import { useState } from 'react'
+
+function Navbar() {
+    
+  
+    return (
+      <>
+        <Button variant='primary'>Primary</Button>
+        <Button variant='secondary'>Secondary</Button>
+        <Button variant='primary'>Primary</Button>
+        <Button variant='secondary'>Secondary</Button>
+      </>
+    )
+  }
+  
+  export default Navbar
+  
