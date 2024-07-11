9d6ac08b4f61947df533e95e46d6579169ba89fa
sakuih <taco@debian.debian.com>
2024-07-11 13:21:14 +0300

    intersection, video smaller

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import 'animate.css'
-
+import { useInView } from 'react-intersection-observer' 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './ImageComponent.css'
 
 function ImageComponent({ src, title, text }) {
+
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  })
   
   return (
-    <div className='fullscreen'>
-      <section className='animate__animated animate__fadeInLeftBig'>
+    <div  >
+      <section ref={ref} className={`element ${inView ? 'animate__animated animate__fadeIn' : 'hidden'} `}>
         <div className='image-container half'>
           <img src={src} alt="image description"/>
         </div>
--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -1,12 +1,19 @@
 import '../App.css'
 import 'animate.css'
+import { useInView } from 'react-intersection-observer'
+import './VideoComponent.css'
 
 function VideoComponent({src}) {
+
+  const { ref, inView } = useInView ({
+    triggerOnce: true,
+    threshold: 0.5,
+  })
 
   return (
 
     <div>
-      <section>
+      <section ref={ref} className={`element ${inView ? 'animate__animated animate__zoomIn' : 'hidden'} `}>
         <video className='video-container ' autoPlay muted loop>
           <source src={src} type="video/mp4" /> 
           Your browser does not support the video tag
