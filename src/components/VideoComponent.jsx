7174428843f7820d0b456fbbdf32c41c581514d6
sakuih <taco@debian.debian.com>
2024-07-16 21:47:33 +0300

    colors changed

--- src/components/VideoComponent.jsx
+++ src/components/VideoComponent.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import '../App.css'
 import 'animate.css'
 import { useInView } from 'react-intersection-observer'
 import './VideoComponent.css'
 
+
 function VideoComponent({src}) {
+
 
   const { ref, inView } = useInView ({
     triggerOnce: true,
@@ -13,8 +16,8 @@ function VideoComponent({src}) {
   return (
 
     <div>
-      <section ref={ref} className={`element ${inView ? 'animate__animated animate__zoomIn' : 'hidden'} `}>
-        <video className='video-container ' autoPlay muted loop>
+      <section ref={ref} className={`element ${inView ? 'animate__animated animate__fadeIn' : 'hidden'} `}>
+        <video className='video-container ' autoPlay muted loop >
           <source src={src} type="video/mp4" /> 
           Your browser does not support the video tag
         </video>
