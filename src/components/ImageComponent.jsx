7e4ee69cdb2f93b682d56c3699dcfd894fa36f44
Majim3 <leogong3@gmail.com>
2024-07-19 14:58:49 +0300

    fix

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -19,32 +19,34 @@ function ImageComponent({ src, title, text }) {
   return (
     <Container>
       {/* Desktop Layout */}
-      <Row>
-        <Col sm={6} lg={{ order: 'last' }} md={{ order: 'first' }}>
-          <div className='text-container half'>
-            <div>
-              <h2>{title}</h2>
-              <p>{text}</p>
+      <section ref={ref} className={`element ${inView ? 'animate__animated animate__fadeIn' : 'hidden'} `}>
+        <Row>
+          <Col sm={6} lg={{ order: 'last' }} md={{ order: 'first' }}>
+            <div className='text-container half'>
+              <div>
+                <h2>{title}</h2>
+                <p>{text}</p>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col md={6} lg={6}  className='mb-4'>
-          <Row>
-            <Col sm={6} xs={6} className='containerImages mt-3'>
-              <BoxShadowComp src={src} />
-            </Col>
-            <Col sm={6} xs={6} className='containerImages mt-3'>
-              <BoxShadowComp src={src} />
-            </Col>
-            <Col sm={6} xs={6} className='containerImages mt-3'>
-              <BoxShadowComp src={src} />
-            </Col>
-            <Col sm={6} xs={6} className='containerImages mt-3'>
-              <BoxShadowComp src={src} />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={6} lg={6} className='mb-4'>
+            <Row>
+              <Col sm={6} xs={6} className='containerImages mt-3'>
+                <BoxShadowComp src={src} />
+              </Col>
+              <Col sm={6} xs={6} className='containerImages mt-3'>
+                <BoxShadowComp src={src} />
+              </Col>
+              <Col sm={6} xs={6} className='containerImages mt-3'>
+                <BoxShadowComp src={src} />
+              </Col>
+              <Col sm={6} xs={6} className='containerImages mt-3'>
+                <BoxShadowComp src={src} />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </section>
     </Container>
   )
 }
