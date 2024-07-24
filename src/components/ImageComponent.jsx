d0f30f422ce37faa4a4803147ac2880b82fd03ed
Majim3 <leogong3@gmail.com>
2024-07-24 20:53:01 +0300

    bug fixed

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -5,7 +5,7 @@ import { Row, Col, Container } from 'react-bootstrap'
 import BoxShadowComp from './BoxShadowComp.jsx'
 import catImage from '../assets/cat.jpg';
 import { useInView } from 'react-intersection-observer'
-import './imageComponent.css'
+import './ImageComponent.css'
 
 function ImageComponent({ src, title, text }) {
 
@@ -16,16 +16,16 @@ function ImageComponent({ src, title, text }) {
     threshold: 0.5,
   })
 
-  const { ref2, inView2 } = useInView({
+  const { ref: ref2, inView: inView2 } = useInView({
     triggerOnce: true,
-    threshold: 0.5,
+    threshold:0.5,
   })
 
   const img = 'https://media.istockphoto.com/photos/test-picture-id996396804?k=20&m=996396804&s=170667a&w=0&h=huj1Gbf6hCATkhOX8EIXekv9eBWZS8DT-RfSh2sIm-0='
   return (
     <Container>
       {/* Desktop Layout */}
-        <Row ref={ref} className={`element ${inView ? 'animate__animated animate__fadeIn' : 'hidden'} `}>
+        <Row>
           <Col sm={6} lg={{ order: 'last' }} md={{ order: 'first' }}>
             <div className='text-container half'>
               <div>
@@ -35,12 +35,12 @@ function ImageComponent({ src, title, text }) {
             </div>
           </Col>
           <Col md={6} lg={6} className='mb-4'>
-            <Row ref={ref2} className={`element ${inView2 ? 'animate__animated animate__fadeIn' : 'hidden'}`} >
-              <Col sm={6} xs={6}  className='containerImages mt-3'>
-                <BoxShadowComp src={img} />
+            <Row>
+              <Col sm={6} xs={6} ref={ref} className={`element ${inView ? 'fadeIn' : 'hidden'} containerImages mt-3`}>
+                <BoxShadowComp src={src} />
               </Col>
 
-              <Col sm={6} xs={6} className='containerImages mt-3'>
+              <Col sm={6} xs={6} ref={ref2} className={`element ${inView2 ? 'fadeIn2' : 'hidden'} containerImages mt-3`}>
                 <BoxShadowComp src={src} />
               </Col>
               <Col sm={6} xs={6} className='containerImages mt-3'>
