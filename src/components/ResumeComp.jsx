efde5836d33d493f23fc9887581547a562a4653a
Majim3 <leogong3@gmail.com>
2024-08-02 14:27:01 +0300

    fix changes

--- src/components/ResumeComp.jsx
+++ src/components/ResumeComp.jsx
@@ -2,18 +2,44 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './ResumeComp.css'
 import { Col, Container,Row } from 'react-bootstrap'
+import { useInView } from 'react-intersection-observer';
 
 const ResumeComp = () => {
+    
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+      })
+    
+      const { ref: ref2, inView: inView2 } = useInView({
+        triggerOnce: true,
+        threshold:0.5,
+      })
+    
+      const { ref: ref3, inView: inView3 } = useInView({
+        triggerOnce: true,
+        threshold:0.5,
+      })
+    
+      const { ref: ref4, inView: inView4 } = useInView({
+        triggerOnce: true,
+        threshold:0.5,
+      })
+    
+
+
+
+
     return (
         <Container className=" mt-5">
             <Row>
-                <Col xs={12} className="text-center">
+                <Col xs={12} ref={ref2} className={`element ${inView2 ? 'fadeIn' : 'hidden'} text-center`}>
                     <h1>Resume</h1>
                     <p>A seasoned Developer with a passion for solutions and robust backend systems.</p>
                 </Col>
             </Row>
 
-            <Row className="mt-5">
+            <Row ref={ref3}  className={`element ${inView3 ? 'fadeIn2' : 'hidden'} mt-5`}>
                 <Col xs={12} className="col-md-6">
                     <div className="resume-section">
                         <div className="circle">
@@ -45,7 +71,7 @@ const ResumeComp = () => {
                 </Col>
             </Row>
 
-            <Row className="mt-5">
+            <Row  ref={ref4}  className={`element ${inView4 ? 'fadeIn3' : 'hidden'} mt-5`}>
                 <div className="col-12">
                     <div className="resume-section">
                         <div className="circle">
